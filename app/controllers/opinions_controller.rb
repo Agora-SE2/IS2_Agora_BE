99cc9dc5f0b76961e381b58371753243c8d541de
asantamariap <asantamariap@unal.edu.co>
2018-03-16 09:35:21 +0000

    verify_authenticity_token

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,4 +1,5 @@
 class OpinionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_opinion, only: [:show, :edit, :update, :destroy]
 
   # GET /opinions
