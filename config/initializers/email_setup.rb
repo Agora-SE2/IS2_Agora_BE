5600146b57c59f1f7eeeda045d0695c0a9a0c547
asantamariap <asantamariap@unal.edu.co>
2018-04-19 17:23:12 +0000

    mail heroku

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'agora-se.herokuapp.com/',
-  user_name:            ENV["asantamariap14"],
+  user_name:            ENV["asantamariap14@gmail.com"],
   password:             ENV["As98041465123"],
   authentication:       'plain',
   enable_starttls_auto: true 
