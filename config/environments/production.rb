5290c5b769e06f9d93f65958ae15e7e03f1af351
asantamariap <asantamariap@unal.edu.co>
2018-04-19 18:14:22 +0000

    mail heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,16 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.action_mailer.default_url_options = { host: 'https://agora-se.herokuapp.com/' }
+  ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'agora-se.herokuapp.com/',
+  user_name:            "se2agora@gmail.com",
+  password:             "Ingenieria",
+  authentication:       'plain',
+  enable_starttls_auto: true 
+}
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
