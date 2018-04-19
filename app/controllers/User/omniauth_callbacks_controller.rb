6d0bbf46fafbafb8434ffcadbcd98095f54f741c
Andrey Herrera Silva <kaherreras@unal.edu.co>
2018-04-19 05:47:34 +0000

    Listo el error que me hizo borrar la base de datos de heroku >:(

--- app/controllers/User/omniauth_callbacks_controller.rb
+++ app/controllers/User/omniauth_callbacks_controller.rb
@@ -1,4 +1,6 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+# frozen_string_literal: true
+
+class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
