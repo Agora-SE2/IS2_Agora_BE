f1289b4e9c01180041b283a652bc53d9d8c5f3ea
Andrey Herrera Silva <kaherreras@unal.edu.co>
2018-04-19 06:34:18 +0000

    Facebook

--- app/controllers/User/omniauth_callbacks_controller.rb
+++ app/controllers/User/omniauth_callbacks_controller.rb
@@ -2,16 +2,18 @@
 
 class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    if request.env["omniauth.auth"].info.email.blank?
+      redirect_to "/user/auth/facebook?auth_type=rerequest&scope=email"
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
+
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      end
     end
   end
+
   def facebook_access_token
   end
 
