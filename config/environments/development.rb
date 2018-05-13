f18d414de2e4797b26f99b50a643ee90049e9eea
Alejo <asantamariap@unal.edu.co>
2018-05-13 17:02:31 -0500

    mailer localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,9 @@ Rails.application.configure do
     :port => 587,
     :domain => "gmail.com",
     :authentication => :login,
-    :user_name => "[asantamariap14]",
+    :user_name => "[se2agora]",
     :password => "[As98041465123]"}
-  
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -52,7 +52,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  
+
   config.action_mailer.default_url_options = { :host => 'localhost' }
 
   # Suppress logger output for asset requests.
