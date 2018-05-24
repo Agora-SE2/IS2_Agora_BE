9955ef2ac4d5d4243f20d583a412661190e996f8
asantamariap <asantamariap@unal.edu.co>
2018-05-24 17:25:40 +0000

    error heroku corregir

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,5 +1,5 @@
 class NewsletterMailer < ApplicationMailer
-  default to: -> { User.pluck(:email) },
+  default to: -> { User.pluck(:email) }
   default from: 'notificaciones@agora-se2.com'
   def newsletter_email
     mail(subject: 'Mira los ultimos 3 proyectos de ley en Ágora')
